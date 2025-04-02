ebfe321443d0a8c553b9b9e23fd9cfd6be249ef5
PiGGer <1455076726@qq.com>
2025-04-02 21:24:36 +0800

    4.2

--- Assets/Scripts/MoveManager.cs
+++ Assets/Scripts/MoveManager.cs
@@ -8,8 +8,20 @@ public class MoveManager : MonoBehaviour
     [SerializeField] private MoveInSpace _movementData;
     public Transform TrackingObject;
     private Vector3 localPosition = Vector3.zero;
+
+    public static MoveManager Instance { get; private set; }
+    private Vector3 CurrentWorldPosition = Vector3.zero;
+
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;  // 第一次 Awake 时赋值
+        }
+        else
+        {
+            Destroy(gameObject);  // 防止重复创建
+        }
         _movementData.InitializeSystem();
     }
 
@@ -31,18 +43,6 @@ public class MoveManager : MonoBehaviour
             _movementData.UpdateOffset(localPosition);
         }
     }
-
-   /* private bool TryGetDevicePosition(out Vector3 position)
-    {
-        var device = InputDevices.GetDeviceAtXRNode(XRNode.CenterEye);
-        if (device.isValid && device.TryGetFeatureValue(CommonUsages.devicePosition, out position))
-        {
-            return true;
-        }
-
-        position = Vector3.zero; // 确保所有路径都有赋值
-        return false;
-    }*/
     private bool TryGetDevicePosition(out Vector3 position)
     {
         var device = InputDevices.GetDeviceAtXRNode(XRNode.CenterEye);
@@ -53,5 +53,22 @@ public class MoveManager : MonoBehaviour
 
         position = Vector3.zero; // 确保所有路径都有赋值
         return false;
+    }
+
+
+    public void OnSceneIn()
+    {
+        CurrentWorldPosition = this.transform.position;
+
+
+
+    }
+
+    public void OnSceneOut()
+    {
+        CurrentWorldPosition = CurrentWorldPosition + localPosition;
+        this.transform.position = CurrentWorldPosition;
+
+
     }
 }
