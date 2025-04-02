92c701e9456f3700f49a0036389842d2336fa465
Sophieblublu <lcfyan14@gmail.com>
2025-04-02 23:09:48 +0800

    part.2

--- Assets/Scripts/Managers/StoryManager.cs
+++ Assets/Scripts/Managers/StoryManager.cs
@@ -1,3 +1,4 @@
+<<<<<<<< Updated upstream:Assets/Scripts/Managers/StoryManager.cs
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,3 +17,23 @@ public class StoryManager : MonoBehaviour
         
     }
 }
+========
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+>>>>>>>> Stashed changes:Assets/Scripts/Managers/DialogueManager.cs
