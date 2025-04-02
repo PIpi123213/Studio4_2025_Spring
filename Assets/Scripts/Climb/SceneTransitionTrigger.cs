f25a27c6f6005476c1eacfab4192bf763cf4c9ad
Wu-Ace <3049118371@qq.com>
2025-04-02 20:34:27 +0800

    场景切换（闪现）

--- Assets/Scripts/Climb/SceneTransitionTrigger.cs
+++ Assets/Scripts/Climb/SceneTransitionTrigger.cs
@@ -1,18 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
+using AmazingAssets.DynamicRadialMasks;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
 
 public class SceneTransitionTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 挂载在 XRGrabInteractable 物体上
+    private                  XRGrabInteractable grabInteractable;
+    // [SerializeField] private GameObject         DissolveEffectTool;
+    [SerializeField] private DRMGameObject drmGameObject;
+    [SerializeField] private OVRPassthroughLayer ptLayer;
+    bool hasTriggered = false;
     void Start()
     {
-        
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        // 订阅 Select Enter 事件
+        grabInteractable.selectEntered.AddListener(OnSelectEnter);
+        drmGameObject.radius = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSelectEnter(SelectEnterEventArgs args)
     {
-        
+        Debug.Log("catch it！");
+        // 例如：改变材质颜色
+        GetComponent<Renderer>().material.color = Color.red;
+        if (!hasTriggered)
+        {
+            StartCoroutine(AnimateRadius());
+            StartCoroutine(AnimateOpacity());
+        }
+    }
+    [SerializeField] float RadiusDuration    = 5f;
+    [SerializeField] float startRadius = 0f;
+    [SerializeField] float endRadius   = 100f;
+
+    private IEnumerator AnimateRadius()
+    {
+
+        float elapsedTime = 0f;
+
+
+        while (elapsedTime < RadiusDuration)
+        {
+            drmGameObject.radius =  Mathf.Lerp(startRadius, endRadius, elapsedTime / RadiusDuration);
+            elapsedTime          += Time.deltaTime;
+            yield return null;
+        }
+
+        hasTriggered = true;
+        drmGameObject.radius = endRadius;
+    }
+    [SerializeField] float opacityDuration    = 5f;
+    [SerializeField] float startOpacity = 1f;
+    [SerializeField] float endOpacity   = 0f;
+    private IEnumerator AnimateOpacity()
+    {
+
+        float elapsedTime = 0f;
+        Debug.Log("AnimateOpacity");
+
+
+        while (elapsedTime < opacityDuration)
+        {
+            ptLayer.textureOpacity =  Mathf.Lerp(startOpacity, endOpacity, elapsedTime / opacityDuration);
+            elapsedTime            += Time.deltaTime;
+            yield return null;
+        }
+
+        hasTriggered           = true;
+        ptLayer.textureOpacity = endOpacity;
+    }
+    void OnDestroy()
+    {
+        // 取消订阅事件
+        grabInteractable.selectEntered.RemoveListener(OnSelectEnter);
     }
 }
