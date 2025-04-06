7a139adff2c80a7054b672bad1a82744784d25d4
PiGGer <1455076726@qq.com>
2025-04-07 01:26:10 +0800

    fix manager issue

--- Assets/Scripts/Managers/StoryManager.cs
+++ Assets/Scripts/Managers/StoryManager.cs
@@ -1,4 +1,3 @@
-<<<<<<<< Updated upstream:Assets/Scripts/Managers/StoryManager.cs
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,23 +16,3 @@ public class StoryManager : MonoBehaviour
         
     }
 }
-========
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DialogueManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
->>>>>>>> Stashed changes:Assets/Scripts/Managers/DialogueManager.cs
