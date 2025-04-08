f308d85b4e465038c3de39fc225e12cbc216b9bc
EgilHu <qingccch@gmail.com>
2025-04-08 22:23:21 +0800

    添加游戏状态机GameStateMachine和事件管理器EventManager

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -1,13 +1,53 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-public static class EventManager
+public class EventManager : MonoBehaviour
 {
-    public static event Action GotoNextPhase;
-    public static void OnGotoNextPhase()
+    public static EventManager Instance { get; private set; }
+
+    // 事件表：事件名 -> 委托
+    private Dictionary<string, Action<object>> eventTable = new Dictionary<string, Action<object>>();
+
+    private void Awake()
     {
-        GotoNextPhase?.Invoke();
+        // 单例初始化
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    /// 注册事件监听
+    public void Subscribe(string eventName, Action<object> listener)
+    {
+        if (!eventTable.ContainsKey(eventName))
+            eventTable[eventName] = delegate { }; // 初始化
+
+        eventTable[eventName] += listener;
+    }
+    
+    /// 移除事件监听
+    public void Unsubscribe(string eventName, Action<object> listener)
+    {
+        if (eventTable.ContainsKey(eventName))
+            eventTable[eventName] -= listener;
+    }
+    
+    /// 触发事件（可传参）
+    public void Trigger(string eventName, object param = null)
+    {
+        if (eventTable.ContainsKey(eventName))
+        {
+            eventTable[eventName]?.Invoke(param);
+        }
+        else
+        {
+            Debug.LogWarning($"事件 {eventName} 未被订阅，但仍被触发。");
+        }
     }
 }
